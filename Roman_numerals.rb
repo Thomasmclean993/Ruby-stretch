3e81850fe1a35a19d7b7d25b9d34ff87f0f57187
Thomas McLean <thomasmclean993@gmail.com>
2021-04-14 12:26:47 -0400

    THis is my first commit!!!

---
+++ Roman_numerals.rb
@@ -0,0 +1,14 @@
+
+number_map = {
+    'one' => 'I',
+    'two' => 'II',
+    'three' => 'III',
+    'four' => 'IV',
+    'five' => 'V',
+    'six' => 'VI',
+    'seven' => 'VII',
+    'eight' => 'VIII',
+    'nine' => 'IX',
+    'ten' => 'X',
+}
+
