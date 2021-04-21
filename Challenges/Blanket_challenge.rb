44edadd391bac53404b07e5f8f9a83100628c18c
Thomas McLean <thomasmclean993@gmail.com>
2021-04-21 12:51:50 -0400

    changes to blanket and guess 2nd

--- Challenges/Blanket_challenge.rb
+++ Challenges/Blanket_challenge.rb
@@ -1,7 +1,24 @@
 blanket = ['!', '@', '#', '$', '%', '^', '&']
-
-loop do
-    puts blanket.shift(1)
-    if blanket = ['&']
-    break 
+lines = 30
+count = 0
+while count < lines
+	start = count.modulo(blanket.length)
+	first_half = blanket[start..-1]
+	second_half = blanket[0...start]
+	puts first_half + second_half
+	count += 1
 end
+# Iterator attempt 
+
+blanket = "!, @, #, $, %, ^, &"
+attempts = 20 
+attempts.times do |i|
+	first = blanket[0]
+	last = blanket[1..-1]
+	blanket = last + first 
+	puts blanket
+end 
+
+
+
+
--- Challenges/Guess_challenge2.rb
+++ Challenges/Guess_challenge2.rb
@@ -5,4 +5,32 @@ puts "|    Guessing Game     |"
 puts " ---------------------- "
 
 puts "What is your name? "
-puts "What "
+puts ">" 
+name = gets.chomps
+
+puts "Hello, #{name}!!"
+puts "Let's play a game."
+puts "I'm going to chose a number between 1 and 10. "
+puts "Guess the number. Easy, right?"
+
+number = rand(10) +1
+puts "I have my number."
+
+MAX_GUESSES = 3
+
+1.upto {MAX_GUESSES} do |guess_num|
+    print "Guess #{guess_num}"
+    guess = gets.chomps
+    when guess.to_i > number
+        puts "Alittle high, try again!!"
+    when guess.to_i < number 
+        puts "Alittle low, Try something higher!!"
+    else 
+        puts "Ding Ding, That is the correct answer."
+    if guess_num == MAX_GUESSES
+        puts 
+        puts "That was your final attempt. "
+        puts "The correct asnwer was #{number}"
+    break
+    end 
+end 
