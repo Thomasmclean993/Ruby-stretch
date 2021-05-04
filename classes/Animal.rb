1e21205fb0bc5b2b95e2048a7f35e20dd531d75e
Thomas McLean <thomasmclean993@gmail.com>
2021-05-04 14:35:40 -0400

    Deleted un needed files

---
+++ classes/Animal.rb
@@ -0,0 +1,11 @@
+class Animal # creates a class called animal. Tells ruby that the animal classes exist
+
+    def make_noise 
+        @noise 
+    end
+
+    def set_noise # this method assigns the instance variable as onik
+        @noise = "Oink!!"
+    end
+end # so when I call make noise, the value pres
+
