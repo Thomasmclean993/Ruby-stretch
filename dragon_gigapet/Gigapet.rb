4e74e5ee0856c7ed3947f1790b630eedfe8a2c01
Thomasmclean993 <69599677+Thomasmclean993@users.noreply.github.com>
2021-05-30 18:17:19 -0400

    Update Gigapet.rb

--- dragon_gigapet/Gigapet.rb
+++ dragon_gigapet/Gigapet.rb
@@ -15,39 +15,40 @@ puts ">>>"
 
 
 
-roll = gets.chomp 
+roll = gets.chomp.to_i
 puts "The color of the egg?"
 color = gets.chomp.downcase
 puts "It's moving, the egg shakes #{roll} times before hatching."
 
+
 case roll
 when 1..5
-    
+    @hungry == true 
 when 6..10
-    @fun -=60
+    bored == true
 when 11..15
-    @energy -=60
+    tired == true
 when 16..19
-    @affection -=60
+    need_affection == true 
 else  
     puts "Lucky you!!"
 end 
 
-#Need to figure out how to have the case statement effect the attriubtes in the class without breaking 
+
 puts "Before your very eyes, a dragon walks out of the shell that was once it's home. A #{color} dragon stares up at you as your stand there in shock"
 puts "To leave the cave with your dragon, You have to convince the dragon to leave with you. "
 
 
 
- 
+ # Ask to see if this method should be it the class animal
 def mood_check(my_dragon) 
-    if my_dragon.hunger < 60
+    if my_dragon.hungry == true
         puts "The dragon seems to try to bite at your leg. They must be hungry?"
-    elsif my_dragon.fun < 60
+    elsif my_dragon.bored == true
         puts "The dragon tries to jump on you? They must want to play. "
-    elsif my_dragon.energy < 60
+    elsif my_dragon.tired == true 
         puts "The dragon attempts to sleep on the ground even though they just hatched. They must be bored?"
-    elsif my_dragon.affection < 60   
+    elsif my_dragon.need_affection   == true    
         puts "The Dragon is rubbing you leg, seems to want somekind of affection?"
     else
         puts "Looks like the dragon is ready to leave!!"
@@ -56,8 +57,9 @@ end
 
   
   
-hatchling = Animal.new(100, 100, 100, 100)  
+hatchling = Animal.new(false, true, false, false) 
 mood_check(hatchling)
+puts hatchling.hungry
 
 =begin
 case 
@@ -72,6 +74,10 @@ when my_dragon.affection < 40
 else
     puts "They start walking towards the exit. Good luck trying to have a dragon as a pet."    
 end
+
+
+case roll
+
 =end
     
 
